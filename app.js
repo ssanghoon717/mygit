d54542a2e3fd48fd8f92027a5e950f9b1490e021
sanghoon <hoon7711@gmail.com>
2016-08-19 17:58:15 +0900

    5:58

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-/*
-app.get('/', function(req, res){
-  res.send('Hello World!');
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://mongo:mongo01@ds051923.mlab.com:51923/mymongo");
+var db = mongoose.connection;
+db.once("open", function(){
+  console.log("DB connected!");
 });
-*/
+db.on("error", function(err){
+  console.log("DB ERROR :", err);
+});
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+var Data = mongoose.model('data', dataSchema);
+Data.findOne({name:"myData"},function(err,data){
+  if(err) return console.log("Data Error:",err);
+  if(!data){
+    Data.create({name:"myData", count:0}, function(err,data){
+      if(err) return console.log("Data Error:", err);
+      console.log("Connter initialized:", data);
+    });
+  }
+});
+
+app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.render('first_ejs!');
+});
+
+
 
 app.listen(3000, function(){
   console.log('Server On!');
